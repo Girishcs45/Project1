776445a68e3c69208245c37316224638d0ef2ecc
Girish Sonawane <sonawanegirish4041@gmail.com>
2025-06-25 19:23:56 +0530

    crud operation of project

---
+++ models/listing.js
@@ -0,0 +1,26 @@
+const mongoose =require("mongoose");
+const Schema = mongoose.Schema;
+const ListingSchema  = new Schema({
+    title : {
+        type : String,
+        required : true,
+    },
+    description : String,
+    image : {
+        filename :{type:String} ,
+        url : {type : String,default : 
+        "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set : (v)=> 
+            v ===" "
+            ? "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            : v ,},
+
+        
+    },
+    price : Number,
+    location : String,
+    country : String,
+});
+
+const Listing = mongoose.model("Listing",ListingSchema);
+module.exports = Listing;
