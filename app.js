8c3958c00fe176bee3baa81124c4b91e9a501f66
Girish Sonawane <sonawanegirish4041@gmail.com>
2025-07-07 00:57:59 +0530

    s8

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js")
 const session = require("express-session");
 const flash = require("connect-flash");
-
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+const userRouter = require("./routes/user.js");
 
 main().then(()=> { 
     console.log("connect to DB");
@@ -44,24 +47,34 @@ app.get("/",(req,res) =>{
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next ) =>{
     res.locals.success=req.flash("success");
+    res.locals.error = req.flash("error");
     next();
 });
 
-
 app.use("/listings", listings);
 app.use("/listings/:id/reviews",reviews);
-
-// app.all("*" , (req,res,next) => {
-//     next(new ExpressError(404,"Page not found!"));
-// });
+app.use("/",userRouter);
 
 
-app.use((err,req,res, next) => {
-   let {statusCode = 500, message = "Something went wrong!"} = err;
-   res.render("error.ejs",{message});
+
+app.use((err, req, res, next) => {
+   let { statusCode = 500, message = "Something went wrong!" } = err;
+   res.status(statusCode).render("error.ejs", {
+       message,
+       error: req.flash("error"), // ✅ this was missing
+       success: req.flash("success") // (optional)
+   });
 });
+
 
 app.listen(8080 , () =>{
     console.log("Server is listening!!");
